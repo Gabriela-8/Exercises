874f6cc0c48ebe0ff39d34a2b5fa62338c06c708
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-05 18:10:25 +0300

    Add files via upload

---
+++ 5 юли/Зарове/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int roll = dice.Next(1, 7);
+            Console.WriteLine(roll);
+        }
+    }
+}
