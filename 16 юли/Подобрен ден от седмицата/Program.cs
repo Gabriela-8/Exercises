240dadd67195a96ceabea993a5e9464d3c68825b
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-16 19:41:25 +0300

    Add files via upload

---
+++ 16 юли/Подобрен ден от седмицата/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace improvedDayOfTheWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = new string[7] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            Console.Write("Please, enter number between 1-7: ");
+            int dayNum = int.Parse(Console.ReadLine());
+
+            if (dayNum > 0 && dayNum < 8)
+            {
+                Console.WriteLine($"{days[dayNum - 1]}");
+            }
+        }
+    }
+}
