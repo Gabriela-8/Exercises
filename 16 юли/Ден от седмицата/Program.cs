ade16eb16d933baa6359b5efa1539f5f1dd58e26
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-16 12:08:46 +0300

    Add files via upload

---
+++ 16 юли/Ден от седмицата/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dayOfTheWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please, enter a day number: ");
+            int dayNum = int.Parse(Console.ReadLine());
+
+            switch (dayNum)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input! Please, enter a day number between 1-7.");
+                    break;
+            }
+        }
+    }
+}
