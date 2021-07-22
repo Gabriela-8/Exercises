8531ee4ad9f84fe3990e1694684ea447d9602481
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-22 07:52:18 +0300

    Add files via upload

---
+++ 22 юли/Избиратели/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("You can vote this year, congrats!");
+            }
+            else if (age==17)
+            {
+                Console.WriteLine("Sorry, you are too young to vote this year. Try it the next year.");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you can`t vote.");
+            }
+        }
+    }
+}
