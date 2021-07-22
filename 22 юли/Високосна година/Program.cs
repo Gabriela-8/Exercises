11990b5ca5eebcdf3ad134991e45a55cb045774c
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-22 07:51:16 +0300

    Add files via upload

---
+++ 22 юли/Високосна година/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter the year you want to check with numeric data type.\nEnter: ");
+            int year = int.Parse(Console.ReadLine());
+
+            if ((year % 4) == 0) 
+            {
+                Console.WriteLine($"The {year} is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine($"The {year} isn`t a leap year.");
+            }
+
+        }
+    }
+}
