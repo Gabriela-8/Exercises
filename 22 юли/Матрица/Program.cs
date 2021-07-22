74331cc40b937b27c1abcb5ada6db38635df381b
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-22 07:55:24 +0300

    Add files via upload

---
+++ 22 юли/Матрица/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[3, 3];
+            int i, j;
+
+            Console.Write("Please, enter a value for the elements in the matrix.\n");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write($"Element - [{i},{j}]: ");
+                    array[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("The matrix is: ");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                    Console.Write($"{array[i,j]}\t");
+                Console.Write("\n");
+            }
+            Console.WriteLine();
+        }
+    }
+}
