5da6fee4f8090ce04506dd34b36be52bd7fc7600
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-22 08:00:04 +0300

    Add files via upload

---
+++ 22 юли/Четно или нечетно/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter your number: ");
+            int num = int.Parse(Console.ReadLine());
+            int result = num % 2;
+
+            if (result == 0) 
+            {
+                Console.WriteLine($"{num} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is odd.");
+            }
+        }
+    }
+}
