6855d1998a75e806c0e6d74c407c7a28f0c71a0a
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-22 07:57:55 +0300

    Add files via upload

---
+++ 22 юли/Равен или не/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equalOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter your first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Please, enter your second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 == num2)
+            {
+                Console.WriteLine($"The first number {num1} and the second number {num2} are equal.");
+            }
+            else
+            {
+                Console.WriteLine($"The first number {num1} and the second number {num2} aren`t equal.");
+            }
+
+        }
+    }
+}
