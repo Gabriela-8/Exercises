a979d273c5c9dbce4f3d5a197311eb83c900d9c4
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-22 07:53:34 +0300

    Add files via upload

---
+++ 22 юли/Класификация по височина/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heightClassification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter your height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            if (height <= 150)
+            {
+                Console.WriteLine("You are a minion. ^^");
+            }
+            else if (height > 150 && height < 170)
+            {
+                Console.WriteLine("You have a normal height.");
+            }
+            else if (height >= 170 && height <= 190)
+            {
+                Console.WriteLine("You are very tall.");
+            }
+            else
+            {
+                Console.WriteLine("You are a mutant!");
+            }
+        }
+    }
+}
