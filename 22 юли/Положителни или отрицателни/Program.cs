ec582ddc318c42abd84aeabca20965b5bfe111d1
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-22 07:56:54 +0300

    Add files via upload

---
+++ 22 юли/Положителни или отрицателни/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace positiveOrNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter your number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num > 0) 
+            {
+                Console.WriteLine($"{num} is positive number.");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is negative number.");
+            }
+        }
+    }
+}
