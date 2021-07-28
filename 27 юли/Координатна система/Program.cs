15751a09c6dd21b5c5a6c5947ed28dffff95d9bb
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-28 16:45:22 +0300

    Add files via upload

---
+++ 27 юли/Координатна система/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coordinateSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter a value for X coordinate: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Please, enter a value for Y coordinate: ");
+            double y = double.Parse(Console.ReadLine());
+
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine($"The coordinate point ({x}, {y}) lies in the 1-st quandrant.");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine($"The coordinate point ({x}, {y}) lies in the 2-nd quandrant.");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine($"The coordinate point ({x}, {y}) lies in the 3-rd quandrant.");
+            }
+            else
+            {
+                Console.WriteLine($"The coordinate point ({x}, {y}) lies in the 4-th quandrant.");
+            }
+        }
+    }
+}
