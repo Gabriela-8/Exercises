dadef6001e306857a571b98a436da94194623a96
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-28 16:42:39 +0300

    Add files via upload

---
+++ 27 юли/Гласна или съгласна/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vowelOrConsonant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter your character: ");
+            char ch = char.Parse(Console.ReadLine().ToLower());
+
+            switch(ch)
+            {
+                case 'a':
+                    Console.WriteLine("The character is vowel.");
+                    break;
+                case 'e':
+                    Console.WriteLine("The character is vowel.");
+                    break;
+                case 'i':
+                    Console.WriteLine("The character is vowel.");
+                    break;
+                case 'o':
+                    Console.WriteLine("The character is vowel.");
+                    break;
+                case 'u':
+                    Console.WriteLine("The character is vowel.");
+                    break;
+                default:
+                    Console.WriteLine("The character is consonant.");
+                    break;
+            }
+        }
+    }
+}
