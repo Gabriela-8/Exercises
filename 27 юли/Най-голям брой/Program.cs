1e3d0f85193fdbf19d4718541e29ef329a96df4a
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-28 16:53:22 +0300

    Add files via upload

---
+++ 27 юли/Най-голям брой/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theLargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter your first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Please, enter your second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Please, enter your third number: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine($"The largest number is {num1}");
+                }
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine($"The largest number is {num2}");
+            }
+            else
+            {
+                Console.WriteLine($"The largest number is {num3}");
+            }
+        }
+    }
+}
