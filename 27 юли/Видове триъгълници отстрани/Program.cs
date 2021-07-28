ff77b9fbda62fb341921f419b0415cb4c829701e
Gabriela-8 <84199641+Gabriela-8@users.noreply.github.com>
2021-07-28 16:39:18 +0300

    Add files via upload

---
+++ 27 юли/Видове триъгълници отстрани/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace typesOfTrianglesBySide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter a value for the 1-st side of triangle: ");
+            double sideA = double.Parse(Console.ReadLine());
+            Console.Write("Please, enter a value for the 2-nd side of triangle: ");
+            double sideB = double.Parse(Console.ReadLine());
+            Console.Write("Please, enter a value for the 3-rd side of triangle: ");
+            double sideC = double.Parse(Console.ReadLine());
+
+            if (sideA == sideB && sideB == sideC)
+            {
+                Console.WriteLine("This triangle is an equilateral.");
+            }
+            else if (sideA == sideB || sideB == sideC || sideC == sideA)
+            {
+                Console.WriteLine("This triangle is an isoceles.");
+            }
+            else
+            {
+                Console.WriteLine("This triangle is a scalene.");
+            }
+        }
+    }
+}
